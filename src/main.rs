63ebbfe5c124e3542cf30a2b9ba10d49e818f683
cosmobrain0 <cosmobrain@outlook.com>
2023-05-26 20:30:24 +0100

    Starting translation to Rust/GGEZ

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+//! Basic hello world example, drawing
+//! to a canvas.
+
+use ggez::event;
+use ggez::graphics::{self, Color, DrawMode, MeshBuilder, Rect};
+use ggez::{Context, GameResult};
+
+struct MainState {
+    canvas: graphics::Canvas,
+    mouse: [f32; 2],
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState {
+            canvas: graphics::Canvas::with_window_size(ctx).unwrap(),
+            mouse: [0.0; 2],
+        };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::set_canvas(ctx, Some(&self.canvas));
+        graphics::clear(ctx, graphics::Color::from((255, 255, 255, 255)));
+
+        let mesh = MeshBuilder::new()
+            .rectangle(
+                DrawMode::fill(),
+                Rect::new(0.0, 0.0, 20.0, 50.0),
+                Color::WHITE,
+            )
+            .unwrap()
+            .build(ctx)
+            .unwrap();
+        graphics::draw(ctx, &mesh, ([50.0, 0.0], Color::RED)).unwrap();
+
+        graphics::set_canvas(ctx, None);
+
+        graphics::draw(
+            ctx,
+            &self.canvas,
+            graphics::DrawParam::new().color(Color::from((255, 255, 255, 255))),
+        )?;
+
+        graphics::present(ctx)?;
+
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("hello_canvas", "ggez");
+    let (mut ctx, event_loop) = cb.build()?;
+
+    let state = MainState::new(&mut ctx)?;
+    event::run(ctx, event_loop, state)
+}
