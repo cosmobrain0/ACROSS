1d6526f4037657be8bc5cb052f32e5cc300a3e0b
cosmobrain0 <cosmobrain@outlook.com>
2023-05-31 10:20:43 +0100

    removed debug code

--- src/main.rs
+++ src/main.rs
@@ -57,8 +57,6 @@ impl MainState {
             .into(),
         ];
         menu.borrow_mut().add_elements(buttons);
-        let (window_width, window_height) = graphics::drawable_size(ctx);
-        println!("Window(x={}, y={})", window_width, window_height);
         let s = MainState {
             canvas: graphics::Canvas::new(
                 ctx,
@@ -90,54 +88,18 @@ impl event::EventHandler<ggez::GameError> for MainState {
         .unwrap();
         graphics::clear(ctx, graphics::Color::from((255, 255, 255, 255)));
 
-        let mouse_position = mouse_position(ctx);
-        let mouse_position = vec2d!(mouse_position.x, mouse_position.y);
-
         self.menu.borrow().draw(ctx);
-        if let UIElement::Button(button) = &self.menu.borrow().elements[1] {
-            if button.is_hovered(mouse_position) {
-                draw_rectangle(ctx, vec2d!(0.0, 0.0), vec2d!(20.0, 20.0), Color::RED);
-            }
-        }
 
         graphics::set_canvas(ctx, None);
-        let (canvas_width, canvas_height) =
-            (self.canvas.width() as f32, self.canvas.height() as f32);
-        let (window_width, window_height) = graphics::drawable_size(ctx);
         graphics::draw(
             ctx,
             &self.canvas,
-            graphics::DrawParam::new().color(Color::from((255, 255, 255, 255))), // .scale([window_width / canvas_width, 1.0]), // .scale([
-                                                                                 //     window_width / SCREEN_WIDTH as f32,
-                                                                                 //     window_height / SCREEN_HEIGHT as f32,
-                                                                                 // ]),
+            graphics::DrawParam::new().color(Color::from((255, 255, 255, 255))),
         )?;
 
         graphics::present(ctx)?;
 
         Ok(())
-    }
-
-    fn key_up_event(
-        &mut self,
-        ctx: &mut Context,
-        keycode: event::KeyCode,
-        _keymods: event::KeyMods,
-    ) {
-        match keycode {
-            event::KeyCode::Space => {
-                let position = mouse_position(ctx);
-                println!("Mouse(x = {}, y = {})", position.x, position.y);
-            }
-            event::KeyCode::Q => {
-                let (canvas_width, canvas_height) =
-                    (self.canvas.width() as f32, self.canvas.height() as f32);
-                let (window_width, window_height) = graphics::drawable_size(ctx);
-                println!("Window(x={window_width}, y={window_height})");
-                println!("Canvas(x={canvas_width}, y={canvas_height})");
-            }
-            _ => (),
-        };
     }
 
     fn mouse_button_up_event(
